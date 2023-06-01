aef5e91fd4f10c31d4e2ac865727f19e609d4aee
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-06-01 09:02:39 +0900

    feat: add users slice and users list page

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postReducer from '../features/posts/postsSlice'
 import userReducer from '../features/users/userSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postReducer,
     users: userReducer,
+    notifications: notificationsReducer,
   },
 })
